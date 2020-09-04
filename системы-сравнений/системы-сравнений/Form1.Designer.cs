f6066f9a2a7326ab641d813adcaf2a5926d6ddaa
SekaPule <66112791+SekaPule@users.noreply.github.com>
2020-09-04 10:57:55 +0300

    Add files via upload

---
+++ системы-сравнений/системы-сравнений/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace системы_сравнений
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(558, 378);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(588, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Кол-во сравнений:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(763, 370);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(664, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Результат";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(588, 114);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 238);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ системы-сравнений/системы-сравнений/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace системы_сравнений
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        int[] c = new int[100];//= { 15, 24, 10 };
+        int[] m = new int[100];// = { 11, 23, 8 };
+        int n;
+        TextBox[] artext1 = new TextBox[100];
+        TextBox[] artext2 = new TextBox[100];
+        Label[] arlabel1 = new Label[100];
+        Label[] arlabel2 = new Label[100];
+        int k = 0;
+        void input()
+        {
+            n = int.Parse(textBox1.Text);
+        }
+        void beg(int i)
+        {
+
+                arlabel1[i] = new Label();
+                arlabel1[i].Text = "x = ";
+                arlabel1[i].Name = string.Format("newLabel1-{0}", i);
+                arlabel1[i].Top = 20 + k;
+                arlabel1[i].Left = 20;
+                panel1.Controls.Add(arlabel1[i]);
+
+
+                artext1[i] = new TextBox();
+                artext1[i].Name = string.Format("newTextBox1-{0}", i);
+                artext1[i].Top = 20 + k;
+                artext1[i].Left = 120;
+                panel1.Controls.Add(artext1[i]);
+
+                arlabel2[i] = new Label();
+                arlabel2[i].Text = "mod";
+                arlabel2[i].Name = string.Format("newLabel2-{0}", i);
+                arlabel2[i].Top = 20 + k;
+                arlabel2[i].Left = 240;
+                panel1.Controls.Add(arlabel2[i]);
+
+                artext2[i] = new TextBox();
+                artext2[i].Name = string.Format("newTextBox2-{0}", i);
+                artext2[i].Top = 20 + k;
+                artext2[i].Left = 370;
+                panel1.Controls.Add(artext2[i]);
+
+                k += 30;
+            
+        }
+        void delete()
+        {
+            panel1.Controls.Clear();
+            k = 0;
+        }
+        int NOD(int a, int b)
+        {
+            while (a > 0 && b > 0)
+
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+
+            return a + b;
+        }
+        void IN()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                c[i] = int.Parse(artext1[i].Text);
+                m[i] = int.Parse(artext2[i].Text);
+                
+            }
+        }
+        void result()
+        {
+            //x = c[] (mod m[])
+            bool flag = false;
+            int M = 1, x0 = 0, x;
+            int[] Mi = new int[n];
+            int[] y = new int[n];
+            input();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                    {
+                        if (NOD(m[i], m[j]) != 1)
+                        {
+                            flag = true;
+                        }
+                    }
+                }
+            }
+             if (flag)
+             {
+                MessageBox.Show("Нет решения , числа после mod не взаимно-простые!");
+            }
+
+            //.. textBox2.Text = "Решение " + (n == 1 ? "сравнения" : "системы сравнений") + " по модулю:\n\n";
+            for (int i = 0; i < n; i++)
+            {
+                M *= m[i];//M = (m1 * m2 *...* mn)
+                //textBox2.Text += "x = " + c[i] + " (mod " + m[i] + ")\n";
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Mi[i] = M / m[i];//Mi = M/m[i]
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m[i]; j++)
+                {
+                    if ((Mi[i] * (j + 1) - 1) % m[i] == 0)
+                    {
+                        y[i] = (j + 1) % m[i];//M1y1 = 1(mod m1) 
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                x0 += Mi[i] * y[i] * c[i];//x0 = (M1*y1*c1+M2*y2*c2+...+Mn*yn*cn)
+            }
+
+            x = x0 % M;//x = x0(mod M)
+
+            // x = x0 % M;//x = x0(mod M)
+            /* textBox3.Text = "\n\nx = " + x0 + " (mod " + M + ")\n";
+             if (M < x0)
+             {
+                 textBox4.Text = "x = " + (x0 - M * (int)(Math.Floor((double)x0 / M))) + " (mod " + M + ")\n";
+             }
+             textBox5.Text = "Ответ: " + x;*/
+            listBox1.Items.Add("x = " + x0 + " (mod " + M + ")\n");
+            if (M < x0)
+            {
+                listBox1.Items.Add("x = " + (x0 - M * (int)(Math.Floor((double)x0 / M))) + " (mod " + M + ")");
+            }
+            listBox1.Items.Add("Ответ: " + x);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            delete();
+            n = int.Parse(textBox1.Text);
+            for (int i = 0; i < n; i++)
+            {
+                beg(i);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            IN();
+            result();
+        }
+    }
+}
